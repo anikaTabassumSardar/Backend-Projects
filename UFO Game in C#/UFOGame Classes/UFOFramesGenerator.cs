9b34565655f5a5f59e3d4541383cf84c42a2cc06
Anika Neela <36943683+anikaTabassumSardar@users.noreply.github.com>
2019-04-18 14:07:27 -0500

    Create UFOFramesGenerator.cs

---
+++ UFO Game in C#/UFOGame Classes/UFOFramesGenerator.cs
@@ -0,0 +1,140 @@
+using System.Collections.Generic;
+using System.Text;
+
+namespace UFOGame
+{
+    class UFOFramesGenerator
+    {
+        #region List of UFO Frames
+        /// <summary>
+        /// Returns a list of UFO frames.
+        /// </summary>
+        /// <returns></returns>
+        public static List<string> UfoFrames()
+        {
+            List<string> ufoFrames = new List<string>();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("                  .\n")
+              .Append("                  |\n")
+              .Append("               .-\"^\"-.\n")
+              .Append("              /_....._\\\n")
+              .Append("          .-\"`         `\"-.\n")
+              .Append("         (  ooo  ooo  ooo  )\n")
+              .Append("          '-.,_________,.-'\n")
+              .Append("               /     \\\n")
+              .Append("              /       \\\n")
+              .Append("             /    O    \\\n")
+              .Append("            /   --|--   \\\n")
+              .Append("           /      |      \\\n")
+              .Append("          /      / \\      \\\n");
+
+            ufoFrames.Add(sb.ToString());
+
+            sb = new StringBuilder();
+            sb.Append("                  .\n")
+              .Append("                  |\n")
+              .Append("               .-\"^\"-.\n")
+              .Append("              /_....._\\\n")
+              .Append("          .-\"`         `\"-.\n")
+              .Append("         (  ooo  ooo  ooo  )\n")
+              .Append("          '-.,_________,.-'\n")
+              .Append("               /     \\\n")
+              .Append("              /   O   \\\n")
+              .Append("             /  --|--  \\\n")
+              .Append("            /     |     \\\n")
+              .Append("           /     / \\     \\\n")
+              .Append("          /               \\\n");
+
+            ufoFrames.Add(sb.ToString());
+
+            sb = new StringBuilder();
+            sb.Append("                  .\n")
+              .Append("                  |\n")
+              .Append("               .-\"^\"-.\n")
+              .Append("              /_....._\\\n")
+              .Append("          .-\"`         `\"-.\n")
+              .Append("         (  ooo  ooo  ooo  )\n")
+              .Append("          '-.,_________,.-'\n")
+              .Append("               /  O  \\\n")
+              .Append("              / --|-- \\\n")
+              .Append("             /    |    \\\n")
+              .Append("            /    / \\    \\\n")
+              .Append("           /             \\\n")
+              .Append("          /               \\\n");
+
+            ufoFrames.Add(sb.ToString());
+
+            sb = new StringBuilder();
+            sb.Append("                  .\n")
+              .Append("                  |\n")
+              .Append("               .-\"^\"-.\n")
+              .Append("              /_....._\\\n")
+              .Append("          .-\"`         `\"-.\n")
+              .Append("         (  ooo  ooo  ooo  )\n")
+              .Append("          '-.,_________,.-'\n")
+              .Append("               /--|--\\\n")
+              .Append("              /   |   \\\n")
+              .Append("             /   / \\   \\\n")
+              .Append("            /           \\\n")
+              .Append("           /             \\\n")
+              .Append("          /               \\\n");
+
+            ufoFrames.Add(sb.ToString());
+
+            sb = new StringBuilder();
+            sb.Append("                  .\n")
+              .Append("                  |\n")
+              .Append("               .-\"^\"-.\n")
+              .Append("              /_....._\\\n")
+              .Append("          .-\"`         `\"-.\n")
+              .Append("         (  ooo  ooo  ooo  )\n")
+              .Append("          '-.,_________,.-'\n")
+              .Append("               /  |  \\\n")
+              .Append("              /  / \\  \\\n")
+              .Append("             /         \\\n")
+              .Append("            /           \\\n")
+              .Append("           /             \\\n")
+              .Append("          /               \\\n");
+
+            ufoFrames.Add(sb.ToString());
+
+            sb = new StringBuilder();
+            sb.Append("                  .\n")
+              .Append("                  |\n")
+              .Append("               .-\"^\"-.\n")
+              .Append("              /_....._\\\n")
+              .Append("          .-\"`         `\"-.\n")
+              .Append("         (  ooo  ooo  ooo  )\n")
+              .Append("          '-.,_________,.-'\n")
+              .Append("               / / \\ \\\n")
+              .Append("              /       \\\n")
+              .Append("             /         \\\n")
+              .Append("            /           \\\n")
+              .Append("           /             \\\n")
+              .Append("          /               \\\n");
+
+
+            ufoFrames.Add(sb.ToString());
+
+            sb = new StringBuilder();
+            sb.Append("                  .\n")
+              .Append("                  |\n")
+              .Append("               .-\"^\"-.\n")
+              .Append("              /_....._\\\n")
+              .Append("          .-\"`         `\"-.\n")
+              .Append("         (  ooo  ooo  ooo  )\n")
+              .Append("          '-.,_________,.-'\n")
+              .Append("               /     \\\n")
+              .Append("              /       \\\n")
+              .Append("             /         \\\n")
+              .Append("            /           \\\n")
+              .Append("           /             \\\n")
+              .Append("          /               \\\n");
+
+            ufoFrames.Add(sb.ToString());
+
+            return ufoFrames;
+        }
+        #endregion
+    }
+}
